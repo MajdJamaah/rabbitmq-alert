ebec0c6e01c030ca070fa4bf59c7b496a73c3e86
Majd Jamaah <majdjamaah@gmail.com>
2020-12-25 12:39:59 +0200

    get gmail smtp to work

--- rabbitmqalert/notifier.py
+++ rabbitmqalert/notifier.py
@@ -14,18 +14,22 @@ class Notifier():
         if self.arguments["email_to"]:
             self.log.info("Sending email notification: \"{0}\"".format(body))
 
-            server = smtplib.SMTP(self.arguments["email_server"], 25)
+            #server = smtplib.SMTP(self.arguments["email_server"], 25)
 
-            if self.arguments["email_ssl"]:
-                server = smtplib.SMTP_SSL(self.arguments["email_server"], 465)
+            #if self.arguments["email_ssl"]:
+            #    server = smtplib.SMTP_SSL(self.arguments["email_server"], 465)
 
-            if self.arguments["email_password"]:
-                server.login(self.arguments["email_from"], self.arguments["email_password"])
+            #if self.arguments["email_password"]:
+            #    server.login(self.arguments["email_from"], self.arguments["email_password"])
 
             recipients = self.arguments["email_to"]
             # add subject as header before message text
             subject_email = self.arguments["email_subject"] % (self.arguments["server_host_alias"] or self.arguments["server_host"], self.arguments["server_queue"])
             text_email = "Subject: %s\n\n%s" % (subject_email, text)
+            server = smtplib.SMTP('smtp.gmail.com')
+            server.starttls()
+            #CHANGE ME!!!!!
+            server.login('GMAIL','PASSWORD')#
             server.sendmail(self.arguments["email_from"], recipients, text_email)
             server.quit()
 
